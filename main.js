d43b866e2cc2119727bd4d618661016d8ee5829b
RusherTheBFDIFan <78630521+RusherTheBFDIFan@users.noreply.github.com>
2022-01-09 20:28:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,28 @@ function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+  	video.size(800,400);
+	video.parent("game_console");
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
 function draw() {
 	game()
 }
 
+function modelLoaded(){
+	console.log("MODEL LOADED BABEH");
+}
 
-
-
-
-
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results)
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+  }
+}
