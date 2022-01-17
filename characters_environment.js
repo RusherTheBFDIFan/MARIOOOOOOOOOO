e43b6803c5f999d3a9820970709ebec9b160b5b6
RusherTheBFDIFan <78630521+RusherTheBFDIFan@users.noreply.github.com>
2022-01-16 20:27:47 -0500

    Update characters_environment.js

--- characters_environment.js
+++ characters_environment.js
@@ -222,6 +222,7 @@ function getCoins(coin,character){
   if( character.overlap(coin) && character.live && coin.get==false){
     character.coins+=1;
     coin.get=true;
+    coin_collect.play();
   };
 }
     
@@ -310,6 +311,7 @@ function manualControl(character){
 /* Movements of character */
 function jumping(character){
 	if( (noseY < 200&&character.live) || (touchIsDown&&character.live) ){
+    mario_jump.play();
 		character.velocity.y+=gameConfig.jump;
 	}
 }
@@ -359,6 +361,7 @@ function StepOnEnemy(obj1,obj2){
 	if(obj1_Right>=obj2_Left&&obj1_Left<=obj2_Right && obj1_Down<=obj2_Up+7 && obj1_Down>=obj2_Up-7 && obj2.live==true && obj2.touching.top){
 		obj2.live=false;
     obj1.killing=30;
+    mario_stomp.play();
     obj1.kills++;
     if(obj1.velocity.y>=gameConfig.jump*0.8){
       obj1.velocity.y=gameConfig.jump*0.8;
@@ -377,6 +380,9 @@ function die(character){
     character.status="dead";
     character.changeAnimation('dead');
     character.velocity.y-=2;
+    if(character.liveNumber>0){
+      mario_die.play();
+    }
 }
 
 // check character status and response to sprite and game status
@@ -388,6 +394,7 @@ function checkStatus(character){
   }
   if(character.live==false && character.liveNumber==0){
     gameConfig.status="gameover"
+    game_over.play();
   }
 
 }
